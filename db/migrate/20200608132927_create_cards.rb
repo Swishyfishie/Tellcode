9f47e3329c360bf9c93c0d4fadfe74651cd6e2e7
Swishyfishie <pclandreiuk@gmail.com>
2020-06-08 14:46:00 +0100

    added migrations

--- db/migrate/20200608132927_create_cards.rb
+++ db/migrate/20200608132927_create_cards.rb
@@ -1,6 +1,12 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
+      t.string :youtube_link
+      t.string :codewars_link
+      t.string :article_link
+      t.string :question
+      t.belongs_to :user
+      t.belongs_to :day
 
       t.timestamps
     end
