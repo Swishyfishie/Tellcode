b0ee08f51119d4184c30168f621d81c532311ea6
Swishyfishie <pclandreiuk@gmail.com>
2020-06-15 15:47:15 +0100

    add before action, require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+    before_action :require_login
+    
+
 
     def current_user
         if session[:user_id]
@@ -7,5 +10,13 @@ class ApplicationController < ActionController::Base
         else
           @current_user = nil
         end
+    end
+
+    def is_logged_in?
+        !!current_user
+    end
+
+    def require_login
+      redirect_to root_path unless is_logged_in?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root 'sessions#welcome'
 
   get 'signup', to: 'users#new', as: 'signup'
+
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
+  
   post 'logout', to: 'sessions#destroy', as: 'logout'
 
 
