3743a769fcf60dbe2354eaec4929ef3aab29dfdd
Swishyfishie <pclandreiuk@gmail.com>
2020-06-15 01:24:43 +0100

    change login/signup methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,45 @@ class UsersController < ApplicationController
     end
   
     def create
-        @user = User.find_by(username: user_params[:username])
-        
-        if @user && @user.authenticate(user_params[:password])
-            session[:user_id] = @user.id
-            redirect_to root_path, notice: "Logged in!"
+        @user = User.find_or_create_by(username: user_params[:username])
+      
+        if @user.valid?
+            user_login(@user)
         else 
-            @user = User.create(user_params)
-            session[:user_id] = @user.id
-            redirect_to root_path, notice: "Signed up and logged in!"
+            user_create(@user)
         end
     end
 
     private 
+  
+    def user_login(user)
+        if user.try(:authenticate, user_params[:password])
+            session[:user_id] = user.id
+            flash[:success] = "Welcome back #{user.username}"
+            redirect_to root_path
+        else
+            flash[:error] = 'Wrong credentials'
+            redirect_to login_path
+        end
+    end
+    
+    
+    def user_create(user)
+        if user_params[:password] == user_params[:password_confirmation]
+            user.password = user_params[:password]
+            user.save
+            flash[:success] = 'Welcome To 100DaysOfCode'
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            flash[:error] = "try again or Login with Github"
+            redirect_to signup_path
+        end
+    end
 
     def user_params
         params.require(:user).permit(:username,:password, :password_confirmation)
     end
+
+
 end
