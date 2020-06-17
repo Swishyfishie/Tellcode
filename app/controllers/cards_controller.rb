ca6116fef7fad28a9a7fe4ddf1a6f67c9dd203b5
Swishyfishie <pclandreiuk@gmail.com>
2020-06-17 03:56:00 +0100

    add before action to protect creating multiple cards on the same day

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,14 +1,16 @@
 class CardsController < ApplicationController
-   
+   before_action :check_card, only: [:create]
+
     def new
         @card = Card.new
         @day = @card.build_day
-        
+            
     end
 
       
     def create
-        @card = current_user.cards.create(card_params)     
+        @card = current_user.cards.create(card_params) 
+
         if @card.save
             number_of_cards = current_user.cards.length   
             day = Day.find_by(id: number_of_cards)
@@ -19,9 +21,17 @@ class CardsController < ApplicationController
         end
     end
 
+    def check_card
+        @last_card = current_user.cards.last
+        if current_user.cards.where(created_at:(Time.now - 24.hours)..Time.now).include?(@last_card)
+            redirect_to root_path
+        end
+    end
+
     private 
 
     def card_params
         params.require(:card).permit(:youtube_link,:codewars_link, :article_link,:question, :day_id, day_attributes: [:comment])    
     end
+
 end
