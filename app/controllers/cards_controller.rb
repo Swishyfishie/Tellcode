64f7d7a5ccf1fdbfa0114419321be6cfc71b97f6
Swishyfishie <pclandreiuk@gmail.com>
2020-06-17 03:10:23 +0100

    change style, chage view to display the correct day number

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,12 +3,16 @@ class CardsController < ApplicationController
     def new
         @card = Card.new
         @day = @card.build_day
+        
     end
 
       
     def create
-        @card = current_user.cards.create(card_params)
+        @card = current_user.cards.create(card_params)     
         if @card.save
+            number_of_cards = current_user.cards.length   
+            day = Day.find_by(id: number_of_cards)
+            day.cards << @card
             redirect_to days_path
         else   
             render :new
