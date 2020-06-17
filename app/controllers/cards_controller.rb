eba6da039f0ef14e8c59f9a1e8a3fa816a9d452d
Swishyfishie <pclandreiuk@gmail.com>
2020-06-17 18:03:21 +0100

    tidy up the code

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -21,17 +21,17 @@ class CardsController < ApplicationController
         end
     end
 
+    
+    private 
+    
+    def card_params
+        params.require(:card).permit(:youtube_link,:codewars_link, :article_link,:question, :day_id, day_attributes: [:comment])    
+    end
+    
     def check_card
         @last_card = current_user.cards.last
         if current_user.cards.where(created_at:(Time.now - 24.hours)..Time.now).include?(@last_card)
             redirect_to root_path
         end
     end
-
-    private 
-
-    def card_params
-        params.require(:card).permit(:youtube_link,:codewars_link, :article_link,:question, :day_id, day_attributes: [:comment])    
-    end
-
 end
